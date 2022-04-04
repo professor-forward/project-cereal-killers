8f88a7a3de83353b00a511897de8a7de0d2ce12d
JohnWick08 <mgao041@uottawa.ca>
2022-04-04 21:17:36 +0800

    Ultra Version

---
+++ Ultra Cupid Arrow/server/index.js
@@ -0,0 +1,159 @@
+const PORT=8000
+
+const express = require('express')
+const{MongoClient} = require('mongodb')
+const {v1:uuidv4}=require('uuid')
+const uri = 'mongodb+srv://1043416763:1043416763@cluster0.xpnja.mongodb.net/Cluster0?retryWrites=true&w=majority'
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.json('hello to my app')
+})
+
+app.post('/signup',async(req,res)=>{
+    const client = new MongoClient(uri)
+    const{email,password}=req.body
+    const generateUserId = uuidv4()
+    const hashpassword = await bcrypt.hash(password,10)
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+        const existingUser = await users.findOne({email})
+
+        if(existingUser){
+            return res.status(409).send('user already exist.please login')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+        const data = {
+            user_id:generateUserId,
+            email:sanitizedEmail,
+            hashed_password:hashpassword
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser,sanitizedEmail,{
+            expiresIn:60*24,
+        })
+
+        res.status(201).json({token,userId:generateUserId})
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.post('/login',async(req,res)=>{
+    const client = new MongoClient(uri)
+    const{email,password}=req.body
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password,user.hashed_password)
+
+        if(user&&correctPassword){
+            const token = jwt.sign(user,email,{
+                expiresIn:60*24
+            })
+            res.status(201).json({token,userId:user.user_id})
+        }
+        res.status(400).send('Invalid Credentials')
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.get('user',async(req,res)=>{
+    const client = new MongoClient(uri)
+    const userId = req.query.userId
+    console.log('userId',userId)
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query={user_id:userId}
+        const user =  await users.findOne(query)
+        res.send(user)
+
+    }finally{
+        await client.close()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/users',async(req,res)=>{
+    const client = new MongoClient(uri)
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    }finally {
+        await client.close()
+    }
+})
+
+
+app.put('/user',async(req,res)=>{
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+        const query={user_id:formData.user_id}
+        const updateDocument={
+            $set:{
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url:formData.url,
+                about:formData.about,
+                matches:formData.matches
+            },
+        }
+        const insertedUsser = await users.updateOne(query,updateDocument)
+        res.send(insertedUsser)
+    } finally{
+        await client.close()
+    }
+})
+
+
+app.listen(PORT,()=>console.log('Server running on port'+PORT))
+
